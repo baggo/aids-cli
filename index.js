d21d675bb57d26ccfe895a607798f2d3df1e48db
prestonkyles <prestonwilliamkyles@gmail.com>
2016-11-11 06:14:41 -0600

    Added copy and paste

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
+
 const aids = require('aids')
 const cmd = require('commander')
+const copy = require('copy-paste')
 const package = require('./package.json')
 let text = '';
 
@@ -11,9 +13,15 @@ cmd
   .option('-o, --offset', 'Output capital letter with an offset')
   .parse(process.argv);
 
-for(let i in cmd.args) {
+for (let i in cmd.args) {
   text += cmd.args[i] + " "
   i++
 }
 
-text != '' ? console.log(aids(text, cmd.offset)) : cmd.help()
+if (text != '') {
+  console.log(aids(text, cmd.offset))
+  copy.copy(aids(text, cmd.offset))
+  process.exit()
+} else {
+  cmd.help()
+}
